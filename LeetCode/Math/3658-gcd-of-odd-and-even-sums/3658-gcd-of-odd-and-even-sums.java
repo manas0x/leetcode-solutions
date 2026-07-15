c276a5b646b8b08fc9ff776e1243b73334f03af7
manas arora <67623029+manas0x@users.noreply.github.com>
2026-07-15 22:43:25 +0530

    Time: 1 ms (83.11%), Space: 42.7 MB (20.94%) - LeetHub - LeetHub

--- LeetCode/Math/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ LeetCode/Math/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,28 +1,14 @@
 class Solution {
     int gcd(int a, int b) {
         while (b != 0) {
-            int remainder = a % b;
+            int t = a % b;
             a = b;
-            b = remainder;
+            b = t;
         }
         return a;
     }
 
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd = 1;
-        int sumEven = 2;
-    
-        int count = 1;
-        int lastOdd = 1;
-        int lastEven = 2;
-        while(count < n){
-            count++;
-            lastOdd+=2;
-            lastEven+=2;
-            sumOdd += lastOdd;
-            sumEven += lastEven;
-        }
-        return gcd(sumOdd , sumEven);
+        return gcd(n * n, n * (n + 1));
     }
-
 }
